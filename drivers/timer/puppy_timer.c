5e1b9ee4687d2d27399a246a80d3b1e330e341c6
anacforcelli <ana.forcelli@lsitec.org.br>
2025-10-31 13:44:44 -0300

    Add UART1 node and timer node

--- drivers/timer/puppy_timer.c
+++ drivers/timer/puppy_timer.c
@@ -1,14 +1,14 @@
-// #include <zephyr/init.h>
+#define DT_DRV_COMPAT caninos_puppy_timer
+
 #include <zephyr/kernel.h>
 #include <zephyr/arch/cpu.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/timer/system_timer.h>
-#include <soc.h>
 #include <zephyr/irq.h>
 
 /* Timer Ctrl Bitfields */
-#define TIMER_CTRL_EN              (1 << 0)            /* Timer Enable Bit */
-#define TIMER_CTRL_PRE(x)          (((x) & 0x07) << 3) /* Prescaler Value */
+#define TIMER_CTRL_EN     (1 << 0)            /* Timer Enable Bit */
+#define TIMER_CTRL_PRE(x) (((x) & 0x07) << 3) /* Prescaler Value */
 
 typedef struct __packed {
 	uint32_t cfg_lo;
@@ -23,7 +23,7 @@ typedef struct __packed {
 	uint32_t reset_hi;
 } puppy_timer_t;
 
-static volatile puppy_timer_t *timer = (puppy_timer_t *)PULP_TIMER_BASE;
+static volatile puppy_timer_t *timer;
 
 static uint32_t accumulated_cycle_count;
 
@@ -32,7 +32,7 @@ static inline int sys_clock_hw_cycles_per_tick(void)
 #ifdef CONFIG_SYS_CLOCK_EXISTS
 	return sys_clock_hw_cycles_per_sec() / CONFIG_SYS_CLOCK_TICKS_PER_SEC;
 #else
-	return 1;	// to avoid division by 0
+	return 1; // to avoid division by 0
 #endif
 }
 
@@ -48,24 +48,6 @@ static void puppy_timer_irq_handler(void *unused)
 #ifdef CONFIG_TICKLESS_IDLE
 #error "Tickless idle not yet implemented for Puppy timer"
 #endif
-
-static int sys_clock_driver_init(void)
-{
-	IRQ_CONNECT(PULP_TIMER_LO_IRQ, 0,
-		    puppy_timer_irq_handler, NULL, 0);
-	irq_enable(PULP_TIMER_LO_IRQ);
-
-	/*
-	 * Initialize timer.
-	 * Reset counter and set timer to generate interrupt
-	 * every sys_clock_hw_cycles_per_tick()
-	 */
-	timer->reset_lo = 1U;
-	timer->cmp_lo = sys_clock_hw_cycles_per_tick() - 1;
-	timer->cfg_lo = 0x15;
-
-	return 0;
-}
 
 /**
  *
@@ -86,6 +68,21 @@ uint32_t sys_clock_elapsed(void)
 	return 0;
 }
 
+#define PUPPY_TIMER_INIT(idx)                                                                      \
+	static int puppy_timer_##idx##_init(const struct device *dev)                              \
+	{                                                                                          \
+		IRQ_CONNECT(DT_INST_IRQN(idx), 0, puppy_timer_irq_handler,                         \
+			    DEVICE_DT_INST_GET(idx), 0);                                           \
+                                                                                                   \
+		irq_enable(DT_INST_IRQN(idx));                                                     \
+		timer = (puppy_timer_t *)DT_INST_REG_ADDR(idx);                                    \
+		timer->reset_lo = 1U;                                                              \
+		timer->cmp_lo = sys_clock_hw_cycles_per_tick() - 1;                                \
+		timer->cfg_lo = 0x15;                                                              \
+                                                                                                   \
+		return 0;                                                                          \
+	}                                                                                          \
+	DEVICE_DT_INST_DEFINE(idx, puppy_timer_##idx##_init, NULL, NULL, NULL, PRE_KERNEL_2,       \
+			      CONFIG_SYSTEM_CLOCK_INIT_PRIORITY, NULL);
 
-SYS_INIT(sys_clock_driver_init, PRE_KERNEL_2,
-	 CONFIG_SYSTEM_CLOCK_INIT_PRIORITY);
+DT_INST_FOREACH_STATUS_OKAY(PUPPY_TIMER_INIT);
